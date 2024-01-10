99f3976281d32583122394d0b3d4571abe1599f7
unknown <wapa9768@gmail.com>
2024-01-10 22:58:34 +0200

    backend: add saveWord and deleteById functions and routes for post and delete

--- backend/database/dbFunctions.js
+++ backend/database/dbFunctions.js
@@ -13,4 +13,39 @@ module.exports = {
       });
     });
   },
+
+  saveWord: (newWord) => {
+    return new Promise((resolve, reject) => {
+      const sql = "INSERT INTO words SET ?";
+
+      pool.query(sql, newWord, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(newWord);
+        }
+      });
+    });
+  },
+
+  deleteById: (id) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        "DELETE FROM words WHERE id = ?",
+        [id],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            if (results.affectedRows > 0) {
+              resolve(results);
+            } else {
+              reject(new Error("not found"));
+            }
+          }
+        }
+      );
+    });
+  },
+
 };
--- backend/routes/words.js
+++ backend/routes/words.js
@@ -14,4 +14,34 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const newWord = {
+      english_word: req.body.english_word,
+      finnish_word: req.body.finnish_word,
+      learning_direction: req.body.learning_direction,
+    };
+
+    const savedWord = await database.saveWord(newWord);
+    res.status(200).json(savedWord);
+  } catch (error) {
+    console.error("Error saving word:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/:myId([0-9]+)", async (req, res) => {
+  try {
+    const id = parseInt(req.params.myId);
+    await database.deleteById(id);
+    res.status(204).end();
+  } catch (error) {
+    if (error.message === "not found") {
+      res.status(404).json({ error: "Word pair not found" });
+    } else {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+});
+
 module.exports = router;
