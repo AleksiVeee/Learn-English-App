80fa1ce325c4b173e832a59087827c078fd8e075
unknown <wapa9768@gmail.com>
2024-01-11 23:52:05 +0200

    backend/routes: add patch route and middleware validation for patch and post routes

--- backend/routes/words.js
+++ backend/routes/words.js
@@ -14,7 +14,73 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.delete("/:myId([0-9]+)", async (req, res) => {
+  try {
+    const id = parseInt(req.params.myId);
+    await database.deleteById(id);
+    res.status(204).end();
+  } catch (error) {
+    if (error.message === "not found") {
+      res.status(404).json({ error: "Word pair not found" });
+    } else {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+});
+
+const validatePatchWords = (req, res, next) => {
+  const { english_word, finnish_word } = req.body;
+  const isString = (value) => typeof value === "string";
+  const isNumericString = (value) => !isNaN(value);
+
+  if (
+    (english_word !== undefined &&
+      (!isString(english_word) || isNumericString(english_word))) ||
+    (finnish_word !== undefined &&
+      (!isString(finnish_word) || isNumericString(finnish_word)))
+  ) {
+    res
+      .status(400)
+      .json({ error: "Invalid data. Words must be non-numeric strings." });
+  } else {
+    next();
+  }
+};
+
+router.patch("/:myId([0-9]+)", validatePatchWords, async (req, res) => {
+  try {
+    const id = parseInt(req.params.myId);
+    const word = await database.partialUpdate(id, req.body);
+    res.status(200).json(word);
+  } catch (error) {
+    if (error.message === "not found") {
+      res.status(404).json({ error: "Word not found" });
+    } else {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+});
+
+const validateWords = (req, res, next) => {
+  const { english_word, finnish_word } = req.body;
+  const isString = (value) => typeof value === "string";
+  const isNumericString = (value) => !isNaN(value);
+
+  if (
+    (english_word !== undefined &&
+      (!isString(english_word) || isNumericString(english_word))) ||
+    (finnish_word !== undefined &&
+      (!isString(finnish_word) || isNumericString(finnish_word)))
+  ) {
+    res
+      .status(400)
+      .json({ error: "Invalid data. Words must be non-numeric strings." });
+  } else {
+    next();
+  }
+};
+
+router.post("/",validateWords, async (req, res) => {
   try {
     const newWord = {
       english_word: req.body.english_word,
@@ -27,20 +93,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error("Error saving word:", error);
     res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-router.delete("/:myId([0-9]+)", async (req, res) => {
-  try {
-    const id = parseInt(req.params.myId);
-    await database.deleteById(id);
-    res.status(204).end();
-  } catch (error) {
-    if (error.message === "not found") {
-      res.status(404).json({ error: "Word pair not found" });
-    } else {
-      res.status(500).json({ error: "Internal server error" });
-    }
   }
 });
 
