76909218cd5589bebf8e5915c958005cfe350b5a
unknown <wapa9768@gmail.com>
2024-01-15 23:26:48 +0200

    Cleared out old styling and add some new css into App.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-/*import "./App.css";*/
-import {Route, Routes} from "react-router-dom"
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import StudentPage from "./components/StudentPage";
 import AdminPage from "./components/AdminPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [wordPairs, setWordPairs] = useState([])
+
+  //Get data from the database and store them in the "wordPairs" state
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get("http://localhost:8080/api/words");
+      setWordPairs(result.data)
+    }
+    getData()
+  }, [])
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin" element={<AdminPage wordPairs={wordPairs} setWordPairs={setWordPairs}/>} />
         <Route path="/student" element={<StudentPage />} />
       </Routes>
     </div>
