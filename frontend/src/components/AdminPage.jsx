7780a41e2a15e10771c39e7c885a7cdd5fc6e204
unknown <wapa9768@gmail.com>
2024-01-15 20:30:30 +0200

    components/AdminPage: add functionalities to component

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -1,22 +1,61 @@
+import axios from "axios";
+import { useState } from "react";
+
 const AdminPage = () => {
+  const [language, setLanguage] = useState("fin");
+  const [finnish, setFinnish] = useState("");
+  const [english, setEnglish] = useState("");
+
+  //Change the state value using select element
+  const handleSelectLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const handleSetFinnish = (e) => {
+    setFinnish(e.target.value);
+  };
+
+  const handleSetEnglish = (e) => {
+    setEnglish(e.target.value);
+  };
+
+  //Add new word to the database
+  const createNewWord = async () => {
+    const newWord = {
+      english_word: english,
+      finnish_word: finnish,
+      learning_direction: language,
+    };
+
+    try {
+      const result = await axios.post(
+        "http://localhost:8080/api/words",
+        newWord
+      );
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h2>Admin Page</h2>
       <div>
         <p>Create word</p>
-        <select name="languages">
-          <option value="eng">English</option>
+        <select name="languages" onChange={handleSelectLanguage}>
           <option value="fin">Finnish</option>
+          <option value="eng">English</option>
         </select>
         <div className="word-original">
-          <label>Word</label>
-          <input />
+          <label>Finnish Word</label>
+          <input onChange={handleSetFinnish} />
         </div>
         <div className="word-translation">
-          <label>Trabslation</label>
-          <input />
+          <label>English Word</label>
+          <input onChange={handleSetEnglish} />
         </div>
-        <button>Add</button>
+        <button onClick={createNewWord}>Add</button>
       </div>
     </div>
   );
