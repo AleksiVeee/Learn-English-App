8e77d52a07e44cbbca9770b118e651cb4a21aa8f
unknown <wapa9768@gmail.com>
2024-01-16 22:36:15 +0200

    add: separeted component WordTest from LearnLanguage and made it into own components

--- frontend/src/components/LearnLanguage.jsx
+++ frontend/src/components/LearnLanguage.jsx
@@ -1,46 +1,7 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-const WordTest = ({ word, setScore, learningDirection }) => {
-  const [userInput, setUserInput] = useState("");
-  const correctTranslationRef = useRef(
-    learningDirection === "eng" ? word.english_word : word.finnish_word
-  );
-
-  //Check if the userInput mathces the correct translation and update the score
-  useEffect(() => {
-    //Check the learningDirection value and render words with matching tag
-    correctTranslationRef.current =
-      learningDirection === "eng" ? word.english_word : word.finnish_word;
-
-    if (
-      userInput.toLocaleLowerCase() ===
-      correctTranslationRef.current.toLocaleLowerCase()
-    ) {
-      setScore((prevScore) => prevScore + 1);
-    }
-
-    if (userInput === "") {
-      setScore((prevScore) => Math.max(0, prevScore - 1));
-    }
-  }, [userInput, learningDirection, word, setScore]);
-
-  //Set userInput value to state
-  const handleInputChange = (e) => {
-    setUserInput(e.target.value);
-  };
-
-  //Conditionally render right language using the learningDirection value
-  return (
-    <div>
-      <h3>
-        {learningDirection === "eng" ? word.finnish_word : word.english_word}
-      </h3>
-      <input onChange={handleInputChange} />
-    </div>
-  );
-};
+import WordTest from "./WordTest";
 
 const LearnLanguage = ({ wordPairs, learningDirection }) => {
   const [words, setWords] = useState([]);
