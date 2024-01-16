d9f46f1aa280da3cd707b07f2351000e6e71507e
unknown <wapa9768@gmail.com>
2024-01-16 20:40:20 +0200

    components/StudentPage.jsx: add links to two different views for user

--- frontend/src/components/StudentPage.jsx
+++ frontend/src/components/StudentPage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 const StudentPage = () => {
   return (
     <div>
       <p>StudentPage</p>
+      <h2>Choose language you want to learn</h2>
+      <Link to="/student/learn-english">English to Finnish</Link>
+      <Link to="/student/learn-finnish">Finnish to English</Link>
     </div>
   );
 };
